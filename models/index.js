fec4938a53b56dd16cb37adf0711348ff41ea593
Nico (Filipu) Guarino <nizoworld@gmail.com>
2021-10-29 09:08:02 -0700

    finished up index.js, not sure if product.belongstomanytag is set up properly

--- models/index.js
+++ models/index.js
@@ -5,12 +5,19 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category);
 
 // Categories have many Products
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'product_tags'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(ProductTag)
 
 module.exports = {
   Product,
