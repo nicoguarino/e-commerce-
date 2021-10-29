1f0e490172ed0c03922f5048ec9214e9fef5572d
Nico (Filipu) Guarino <nizoworld@gmail.com>
2021-10-29 08:57:09 -0700

    finished up tag.js and producttag.js in models folder

--- models/Product.js
+++ models/Product.js
@@ -36,7 +36,10 @@ Product.init(
     },
     category_id: {
       type: DataTypes.INTEGER,
-      references: category_id
+      references: {
+        model: 'category',
+        key: 'id'
+      }
     }
   },
   {
